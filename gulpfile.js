3585ff593aa9468f74894f1c9e7ddaf3c095d6b8
Dominic Scheirlinck <dominic@varspool.com>
2015-11-09 10:58:59 +0000

    More abstraction and better hinting

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var shell        = require('gulp-shell');
 var gulpif       = require('gulp-if');
 var lazypipe     = require('lazypipe');
+var eslint        = require('gulp-eslint');
 
 /*
  * Main Settings
@@ -26,11 +27,14 @@ var debug = (process.env.NODE_ENV == 'development');
 var config = {
     paths: {
         server:          'bin/www',
-        clientSource:    './client/app.jsx',
+        serverSource:    ['*.js', 'routes/*.js', 'gulpfile.js'],
+        client:          './client/app.jsx',
+        clientSource:    ['client/**/*.js'],
+        clientJsxSource: ['client/**/*.jsx'],
         clientCompiled:  'app.js',
         clientOutputDir: './public/js/',
-        scssSource:      './scss/*.scss',
-        cssOutputDir:    './public/css',
+        scssSource:      ['./scss/*.scss'],
+        cssOutputDir:    './public/css'
     },
 
     linting:   debug,
@@ -47,13 +51,28 @@ var pipes = {
                 .pipe(jshint)
                 .pipe(jshint.reporter, 'jshint-stylish'),
 
+    jsLint: lazypipe()
+                .pipe(eslint)
+                .pipe(eslint.format),
+
+    scss: lazypipe()
+              .pipe(sass, {
+            outputStyle: config.compress ? 'compressed' : 'expanded'
+        })
+              .pipe(autoprefixer, {
+            browsers: ['last 2 versions'],
+            cascade:  false
+        })
 };
 
 var sources = {
-    js: () => browserify({
-            entries: config.paths.clientSource,
-            debug:   debug
-        }).transform(reactify)
+    jsClient: () => browserify({
+        entries: config.paths.clientEntry,
+        debug:   debug
+    }).transform(reactify),
+    jsClientSource: () => gulp.src(config.paths.clientSource),
+    jsServer: () => gulp.src(config.paths.serverSource),
+    scss:     () => gulp.src(config.paths.scssSource)
 };
 
 /*
@@ -62,6 +81,7 @@ var sources = {
 
 gulp.task('default', ['build']);
 gulp.task('build', ['js', 'css']);
+gulp.task('js', ['jsClientBuild', 'jsClientSource', 'jsServer']);
 
 gulp.task('run:docker', shell.task([
     'docker run --env-file=.env .'
@@ -71,33 +91,39 @@ gulp.task('run:shell', shell.task([
     config.paths.server
 ]));
 
-gulp.task('js', function () {
-    return sources.js()
+gulp.task('jsClientBuild', function () {
+    return sources.jsClient()
         .bundle()
         .pipe(source(config.paths.clientCompiled))
         .pipe(buffer())
         .pipe(gulpif(config.sourcemap, sourcemaps.init({loadMaps: true})))
         .pipe(gulpif(config.compress, uglify(), beautify()))
-        .pipe(gulpif(config.linting, pipes.jsHint()))
         .on('error', gutil.log)
         .pipe(gulpif(config.sourcemap, sourcemaps.write('./')))
         .pipe(gulp.dest(config.paths.clientOutputDir))
 });
 
+gulp.task('jsClientSource', function () {
+    return sources.jsClientSource()
+        .pipe(gulpif(config.linting, pipes.jsLint()))
+        .pipe(gulpif(config.linting, pipes.jsHint()))
+});
+
+gulp.task('jsServer', function () {
+    return sources.jsServer()
+        .pipe(gulpif(config.linting, pipes.jsLint()))
+});
+
 gulp.task('css', function () {
-    gulp.src(config.paths.scssSource)
+    return sources.scss()
         .pipe(gulpif(config.sourcemap, sourcemaps.init()))
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade:  false
-        }))
+        .pipe(pipes.scss())
         .pipe(gulpif(config.sourcemap, sourcemaps.write()))
         .pipe(gulp.dest(config.paths.cssOutputDir));
 });
 
 gulp.task('watch', ['build'], function () {
-    var server = gls(app);
+    var server = gls(config.paths.server);
     server.start();
 
     var notify = function (file) {
@@ -108,8 +134,8 @@ gulp.task('watch', ['build'], function () {
     gulp.watch(['public/*.html'], notify);
     gulp.watch(['public/images/*'], notify);
 
-    gulp.watch([config.paths.scssSource], ['css', notify]);
-    gulp.watch(['client/**/*.js', 'client/**/*.jsx'], ['js', notify]);
+    gulp.watch(config.paths.scssSource, ['css', notify]);
+    gulp.watch([config.paths.clientSource, config.paths.clientJsxSource], ['js', notify]);
 
     gulp.watch(['*.js', 'routes/**/*.js'], ['build', function () {
         server.stop().then(function () {
