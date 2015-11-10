899339cb9d34aa2a1933cb22db68b1a58aff4221
Dominic Scheirlinck <dominic@varspool.com>
2015-11-10 05:28:24 +0000

    Compress static assets, support tiny-lr

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
+var compress     = require('compression');
+var tinylr       = require('tiny-lr');
 
 var app = express();
 
@@ -15,8 +17,10 @@ app.set('view engine', 'jade');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(compress());
+app.use(tinylr.middleware({ app: app }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // error handlers
@@ -24,23 +28,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error:   err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error:   {}
+    });
 });
 
 module.exports = app;
