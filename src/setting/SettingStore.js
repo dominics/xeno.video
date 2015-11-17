eb1b9c4c866579971677cfeb868361eed0a48ffa
Dominic Scheirlinck <dominic@varspool.com>
2015-11-17 05:44:57 +0000

    Working SettingStore

--- src/setting/SettingStore.js
+++ src/setting/SettingStore.js
@@ -1,9 +1,25 @@
-
+import Promise from 'bluebird';
+import { Map } from 'immutable';
+import _ from 'lodash';
 
 export default class SettingStore {
   redis = null;
 
   constructor(redis) {
     this.redis = redis;
+  }
+
+  getAll(req) {
+    return new Promise((resolve, reject) => {
+      const state = new Map({
+        nsfw: true,
+        ratio: 'free',
+        authenticated: req.isAuthenticated(),
+        userId: _.get(req, 'session.passport.user.reddit.id', null),
+        name: _.get(req, 'session.passport.user.reddit.name', null),
+      });
+
+      return resolve(state.map((v, k) => ({ 'id': k, 'value': v })).toArray());
+    });
   }
 }
