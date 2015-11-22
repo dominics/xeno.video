32de7054fe1cae54833afb9d534e443912e6dc59
Dominic Scheirlinck <dominic@varspool.com>
2015-11-22 13:20:49 +0000

    Working token refresh

--- src/session/validator.js
+++ src/session/validator.js
@@ -1,5 +1,6 @@
 import libdebug from 'debug';
 import _ from 'lodash';
+import Promise from 'bluebird';
 
 const debug = libdebug('xeno:session:validator');
 
@@ -11,20 +12,21 @@ function doRefresh(refresh, req) {
     const refreshToken = _.get(req, 'session.passport.user.refreshToken', null);
 
     if (!refreshToken) {
-      return reject('fail.no_refresh_token');
+      return reject(new Error('fail.no_refresh_token'));
     }
 
     debug('Refreshing access token');
 
     refresh.requestNewAccessToken('reddit', refreshToken, (err, accessToken, newRefreshToken) => {
       if (err) {
-        return reject('fail.refresh: ' + err);
+        debug('Failed to get new access token', err);
+        return reject(err);
       }
 
-      console.log('Got new access token', accessToken);
-      debug('Got new access token', accessToken);
-
-      // STORE TO USER
+      debug('Successfully refreshed access token');
+      req.session.passport.user.accessToken = accessToken;
+      req.session.passport.user.refreshToken = newRefreshToken;
+      req.session.passport.user.authenticated = Date.now() / 1000;
 
       return resolve();
     });
@@ -32,45 +34,46 @@ function doRefresh(refresh, req) {
 }
 
 function doValidate(refresh, authRequired, req) {
-  return new Promise((resolve, reject) => {
-    if (!req.isAuthenticated()) {
-      return authRequired ? reject('fail.auth') : resolve();
-    }
+  if (!req.isAuthenticated()) {
+    return authRequired ? Promise.reject(new Error('fail.auth')) : Promise.resolve();
+  }
 
-    const accessToken = _.get(req, 'session.passport.user.accessToken', null);
-    const authenticated = _.get(req, 'session.passport.user.authenticated', null);
-    const age = (Date.now() / 1000) - authenticated;
+  const accessToken = _.get(req, 'session.passport.user.accessToken', null);
+  const authenticated = _.get(req, 'session.passport.user.authenticated', null);
+  const age = (Date.now() / 1000) - authenticated;
 
-    if (!accessToken || age > (MAX_AGE - PREEMPT_REFRESH)) {
-      return doRefresh(refresh, req);
-    }
+  if (!accessToken || age > (MAX_AGE - PREEMPT_REFRESH)) {
+    return doRefresh(refresh, req);
+  }
 
-    if (age > MAX_AGE) {
-      return reject('fail.session_age');
-    }
+  if (age > MAX_AGE) {
+    return Promise.reject(new Error('fail.session_age'));
+  }
 
-    return resolve();
-  });
+  return Promise.resolve('pass');
 }
 
 function middleware(refresh, failureHandler, authRequired, req, res, next) {
   return doValidate(refresh, authRequired, req)
     .then(() => {
       next();
+      return null;
     })
-    .catch(failureHandler);
+    .catch(failureHandler.bind(undefined, req, res, next));
 }
 
 export default (refresh) => {
   return {
     validate: doValidate.bind(undefined, refresh),
-    api: middleware.bind(undefined, refresh, (err) => {
+
+    api: middleware.bind(undefined, refresh, (req, res, next, err) => {
       res.sendStatus(401);
       res.end();
 
       return next(err);
     }),
-    interactive: middleware.bind(undefined, refresh, (err) => {
+
+    interactive: middleware.bind(undefined, refresh, (req, res, next, err) => {
       req.logout();
       res.redirect('/login');
       res.end();
