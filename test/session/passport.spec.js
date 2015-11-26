001fb180f4714afedb763adfb210ca96a40cce24
Dominic Scheirlinck <dominic@varspool.com>
2015-11-26 10:21:59 +0000

    Fix test to expect async session validation

--- test/session/passport.spec.js
+++ test/session/passport.spec.js
@@ -1,6 +1,6 @@
 import passport from './../../dist/session/passport';
 
-describe('module passport', () => {
+describe('session module passport', () => {
   describe('export default()', () => {
     it('is a passport object constructor', () => {
       expect(passport).to.be.a('function');
--- test/util/validation.spec.js
+++ test/util/validation.spec.js
@@ -28,27 +28,4 @@ describe('util module validation', () => {
       });
     });
   });
-
-  describe('export session()', () => {
-    it('is a validation method', () => {
-      expect(validation.session).to.be.a('function');
-    });
-
-    it('validates a request is authenticated', () => {
-      const mock = {
-        isAuthenticated: sinon.stub().returns(true),
-        session: {
-          passport: {
-            user: {
-              accessToken: 'fooAccessToken',
-              refreshToken: 'fooRefreshToken',
-              authenticated: (Date.now() / 1000) - 10,
-            },
-          },
-        },
-      };
-
-      expect(validation.session(mock)).to.be.eql('pass');
-    });
-  });
 });
