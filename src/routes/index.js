94dfca28fd4ceabe4e4429464f6a5ee762f29f8d
Dominic Scheirlinck <dominic@varspool.com>
2019-11-20 23:18:48 +1300

    Include GOOGLE_ANALYTICS_ID in template vars

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,23 @@
 import libdebug from 'debug';
 const debug = libdebug('xeno:routes:index');
 
-export default (sessionValidator) => (router) => {
+export default (sessionValidator, config) => (router) => {
   const auth = sessionValidator.interactive.bind(undefined, false); // auth not required
 
+  const vars = (existing) => Object.assign({
+    google_analytics_id: config.GOOGLE_ANALYTICS_ID
+  }, existing);
+
   router.get('/', auth, (req, res) => {
-    res.render('index', {
+    res.render('index', vars({
       title: 'xeno.video',
-    });
+    }));
   });
 
   router.get('/about', (req, res) => {
-    res.render('about', {
+    res.render('about', vars({
       title: 'about xeno.video',
-    });
+    }));
   });
 
   return router;
